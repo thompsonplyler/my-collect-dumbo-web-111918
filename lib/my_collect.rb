f0f846d3245709b3095fc35baaa6ecc9f35c324b
Thompson Plyler <nealok.g@gmail.com>
2018-11-07 23:45:07 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,11 @@
 def my_collect(collection)
 i = 0
-collect = []
+collect = []  # the hard part. Iteration has to have somewhere to go since it's not 
+              # just being spit out like with each. 
 
   while i < collection.length
-    collect << yield(collection[i])
-    i +=1
+    collect << yield(collection[i]) # the push was easy after seeing the solution to #my_each
+    i +=1                           # but I still don't grasp where the yield is going. 
   end
 
   collect
