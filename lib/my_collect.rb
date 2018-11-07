00103a9047d2091aea86abc1685e03965bc2c47d
Thompson Plyler <nealok.g@gmail.com>
2018-11-07 23:39:49 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(collection)
+i = 0
+collect = []
 
+  while i < collection.length
+    collect << yield(collection[i])
+    i +=1
+  end
 
+  collect
+
+end
+############################################################
+
+# It should take in an argument of a collection,
+# iterate over that collection using a while loop,
+# and execute the code in the block you call it with
+# for each element in the collection (use the yield keyword).
+# It should return the modified collection.
